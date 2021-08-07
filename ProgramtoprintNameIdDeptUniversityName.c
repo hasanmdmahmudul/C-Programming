9084520e2f54dd062a3e32230acd9cbd04c35d00
Md Mahmudul Hasan <51690967+hasanmdmahmudul@users.noreply.github.com>
2021-08-08 01:48:03 +1000

    Create ProgramtoprintNameIdDeptUniversityName.c

---
+++ ProgramtoprintNameIdDeptUniversityName.c
@@ -0,0 +1,9 @@
+//Program to print Name;Id;Dept;University name
+#include<stdio.h>
+main()
+{
+    printf("My name is MUBIN\n");
+    printf("My ID is 1103043\n");
+    printf("My Dept. is EEE\n");
+    printf("My University is BAUET\n");
+}
