a4c3d3daea29f816bca004314c231de3a48de4fa
Md Mahmudul Hasan <51690967+hasanmdmahmudul@users.noreply.github.com>
2021-08-08 01:49:52 +1000

    Create Keyboard_Input.c

---
+++ Keyboard_Input.c
@@ -0,0 +1,12 @@
+//Add two numbers (take input from keyboard)
+#include<stdio.h>
+main()
+{
+    int a,b,sum;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("Sum= %d + %d = %d\n",a,b,sum);
+}
